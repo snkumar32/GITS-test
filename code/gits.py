c9a25911a1131f9c3f453c8eb397426facbce674
snkumar32 <snkumar1912@gmail.com>
2020-09-20 00:27:16 -0400

    updated gits.py

--- code/gits.py
+++ code/gits.py
@@ -6,11 +6,9 @@ from gits_pr_update import gits_pr_update
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers()
 
-
-gits_pr_subparser= subparsers.add_parser('gits_pr_update')
-
+gits_pr_subparser= subparsers.add_parser('sync', help='sync help')
 gits_pr_subparser.set_defaults(func=gits_pr_update)
-
+gits_pr_subparser.add_argument('--upstream', nargs='?')
 args = parser.parse_args()
 args.func(args)
 
--- code/gits_pr_update.py
+++ code/gits_pr_update.py
@@ -3,14 +3,30 @@ import sys
 import subprocess
 import argparse
 
-
 def gits_pr_update(args):
+    print(args)
+    print("Hello from GITS command line tools- PR Update")
+    upstream_check = os.popen("git remote -vv | grep upstream").read()
+    print(upstream_check)
+    if upstream_check:
+        print("Upstream set")
+    elif not upstream_check and args.upstream:
+        print("Setting upstream")
+        rc = os.popen("git remote add upstream " + args.upstream)
+        print(os.popen("git remote -vv | grep upstream").read())
+    else:
+        print("Set --upstream")
+        exit()
+
+    checkout = os.popen("git checkout master").read()
+    upstream_pull = os.popen("git pull upstream master").read()
+    origin_push = os.popen("git push origin master").read()
 
 
-        #print(args)
-        #print("Hello from GITS command line tools- PR Update")
-        #subprocess.call(["git checkout master"],shell=True)
-        #subprocess.call(["git pull upstream master"], shell = True)
+
+
+    
+
 
 
 
