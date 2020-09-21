85b57854fb9a2d635f75764836a030a7d5bf751e
snkumar32 <snkumar1912@gmail.com>
2020-09-21 03:45:45 -0400

    gits sync

--- code/gits_pr_update.py
+++ code/gits_pr_update.py
@@ -2,32 +2,67 @@ import os
 import sys
 import subprocess
 import argparse
+from subprocess import Popen, PIPE
+
 
 def gits_pr_update(args):
-    print(args)
+   #print(args)
     print("Hello from GITS command line tools- PR Update")
-    upstream_check = os.popen("git remote -vv | grep upstream").read()
-    print(upstream_check)
-    if upstream_check:
-        print("Upstream set")
-    elif not upstream_check and args.upstream:
-        print("Setting upstream")
-        rc = os.popen("git remote add upstream " + args.upstream)
-        print(os.popen("git remote -vv | grep upstream").read())
-    else:
-        print("Set --upstream")
-        exit()
 
+    flag = 0
+    try:
+        Untracked_file_check_status = list()
+        Untracked_file_check_status.append("git")
+        Untracked_file_check_status.append("status")
+        Untracked_file_check_status.append("--porcelain")
 
-    checkout = os.popen("git checkout master").read()
-    upstream_pull = os.popen("git pull upstream master").read()
-    origin_push = os.popen("git push origin master").read()
+        #Untracked_file_check = list()
+        #Untracked_file_check.append("grep")
+        #Untracked_file_check.append("Untracked files")
 
+        process1 = subprocess.Popen(Untracked_file_check_status, stdout=PIPE, stderr=PIPE)
+        #process11 = subprocess.Popen(Untracked_file_check, stdin=process1.stdout, stdout=PIPE, stderr=PIPE)
 
+        stdout, stderr = process1.communicate()
+        print(format(stdout))
+        if (stdout != b''):
+            print("Note: Please commit uncommitted changes")
+            #git stash
+            exit()
 
+        if flag == 0:
+            print("Checking if upstream is set...")
+            process2 = subprocess.Popen(['git', 'remote', '-vv'], stdout=PIPE, stderr=PIPE)
+            process21 = subprocess.Popen(['grep', 'upstream'], stdin=process2.stdout,
+                                     stdout=PIPE, stderr=PIPE)
+            stdout, stderr = process21.communicate()
 
-    
+            if stdout is not None:
+                 print("Upstream set")
+            elif not stdout and args.upstream:
+                print("Setting upstream")
+                process3 = subprocess.Popen(['git', 'remote', 'add', 'upstream'], stdout=PIPE, stderr=PIPE)
+                stdout, stderr = process3.communicate()
+            else:
+                print("Set --upstream")
+                exit()
 
+            process4 = subprocess.Popen(['git', 'checkout', 'master'], stdout=PIPE, stderr=PIPE)
+            stdout, stderr = process4.communicate()
+            print(stdout)
+            process5 = subprocess.Popen(['git', 'pull', 'upstream', 'master'], stdout=PIPE, stderr=PIPE)
+            stdout, stderr = process5.communicate()
+            print(stdout)
+            process6 = subprocess.Popen(['git', 'push', 'origin', 'master'], stdout=PIPE, stderr=PIPE)
+            stdout, stderr = process6.communicate()
+            print(stdout)
 
+        else:
+            exit()
 
+    except Exception as e:
+        print("ERROR: gits sync command caught an exception")
+        print("ERROR: {}".format(str(e)))
+        return False
 
+    return True
