29a53d03c31f4f9104a74098785209667445146a
snkumar32 <snkumar1912@gmail.com>
2020-09-21 03:13:06 -0400

    profile & sync

--- code/gits.py
+++ code/gits.py
@@ -1,14 +1,20 @@
 import os
 import sys
 import argparse
-from gits_pr_update import gits_pr_update
+from gits_profile import gits_set_profile
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers()
 
-gits_pr_subparser= subparsers.add_parser('sync3335', help='sync help')
+gits_profile_subparser = subparsers.add_parser('profile', help='profie help')
+gits_profile_subparser.set_defaults(func=gits_set_profile)
+gits_profile_subparser.add_argument('--email', required=True, help='email to be used')
+gits_profile_subparser.add_argument('--name', required=True, help='name to be used')
+#print(gits_profile_subparser.parse_args(['--email']))
+
+gits_pr_subparser= subparsers.add_parser('sync', help='sync help')
 gits_pr_subparser.set_defaults(func=gits_pr_update)
 gits_pr_subparser.add_argument('--upstream', nargs='?')
+
 args = parser.parse_args()
 args.func(args)
-
--- code/gits_pr_update.py
+++ code/gits_pr_update.py
@@ -2,32 +2,65 @@ import os
 import sys
 import subprocess
 import argparse
+from subprocess import Popen, PIPE
+
 
 def gits_pr_update(args):
     print(args)
     print("Hello from GITS command line tools- PR Update")
-    upstream_check = os.popen("git remote -vv | grep upstream").read()
-    print(upstream_check)
-    if upstream_check:
-        print("Upstream set")
-    elif not upstream_check and args.upstream:
-        print("Setting upstream")
-        rc = os.popen("git remote add upstream " + args.upstream)
-        print(os.popen("git remote -vv | grep upstream").read())
-    else:
-        print("Set --upstream")
-        exit()
 
+    flag = 0
+    try:
+        Untracked_file_check_status = list()
+        Untracked_file_check_status.append("git")
+        Untracked_file_check_status.append("status")
 
-    checkout = os.popen("git checkout master").read()
-    upstream_pull = os.popen("git pull upstream master").read()
-    origin_push = os.popen("git push origin master").read()
+        Untracked_file_check = list()
+        Untracked_file_check.append("grep")
+        Untracked_file_check.append("Untracked files")
 
+        process1 = subprocess.Popen(Untracked_file_check, stdout=PIPE, stderr=PIPE)
+        process11 = subprocess.Popen(Untracked_file_check_status, stdin=process1.stdout,
+                                         stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process11.communicate()
 
+        if (stdout is not None):
+            print("Caution u have uncommited changes")
+            flag = 1
+             # git stash can be implemented at this point
 
+        print("Note: Please commit uncommited changes. Would you like to continue with PR-udate?")
+        input1 = input("[yes/no]")
+        if flag == 0:
 
-    
+            process2 = subprocess.Popen(['git', 'remote', '-vv'], stdout=PIPE, stderr=PIPE)
+            process21 = subprocess.Popen(['grep', 'upstream'], stdin=process1.stdout,
+                                     stdout=PIPE, stderr=PIPE)
+            stdout, stderr = process21.communicate()
 
+            if stdout is not None:
+                 print("Upstream set")
+            elif not stdout and args.upstream:
+                print("Setting upstream")
+                process3 = subprocess.Popen(['git', 'remote', 'add', 'upstream'], stdout=PIPE, stderr=PIPE)
+                stdout, stderr = process3.communicate()
+            else:
+                print("Set --upstream")
+                exit()
 
+            process4 = subprocess.Popen(['git', 'checkout', 'master'], stdout=PIPE, stderr=PIPE)
+            stdout, stderr = process4.communicate()
+            process5 = subprocess.Popen(['git', 'pull', 'upstream', 'master'], stdout=PIPE, stderr=PIPE)
+            stdout, stderr = process5.communicate()
+            process6 = subprocess.Popen(['git', 'push', 'origin', 'master'], stdout=PIPE, stderr=PIPE)
+            stdout, stderr = process6.communicate()
 
+        else:
+            print("message")
 
+    except Exception as e:
+        print("ERROR: gits sync command caught an exception")
+        print("ERROR: {}".format(str(e)))
+        return False
+
+    return True
